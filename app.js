2a86def7e3d07089f5cf95b3274fc1248e9a7fd2
Julian Currie <julian.currie@gmail.com>
2017-02-15 08:15:26 +0000

    added query param GET

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.get('/about', function(req, res) {
     res.render("about")
 });
 
+app.get('/:query', function(req, res) {
+    res.send("Welcome to the " + req.params.query + " page!");
+});
+
 app.listen(process.env.PORT, process.env.IP);
